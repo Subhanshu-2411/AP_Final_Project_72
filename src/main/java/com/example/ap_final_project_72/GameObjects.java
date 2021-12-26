c8b7b452db108fbcaa2c3f447f2b1c0fdc06552e
Subhanshu Bansal <77500658+Subhanshu-2411@users.noreply.github.com>
2021-12-26 12:31:11 +0530

    Delete src/main/java/com/example/ap_final_project_72 directory

--- src/main/java/com/example/ap_final_project_72/GameObjects.java
+++
@@ -1,4 +0,0 @@
-package com.example.ap_final_project_72;
-
-public interface GameObjects {
-}
--- src/main/java/com/example/ap_final_project_72/Obstacles.java
+++
@@ -1,8 +0,0 @@
-package com.example.ap_final_project_72;
-
-public class Obstacles implements GameObjects{
-
-    protected double width;
-    protected double height;
-
-}
